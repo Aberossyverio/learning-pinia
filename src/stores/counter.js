969cd82c1f2deb9e488e4b73df915f82433ac5b9
Verio Aberossy R <94156647+Aberossyverio@users.noreply.github.com>
2023-10-09 21:30:16 +0700

    update store

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,24 +1,32 @@
 import { defineStore } from "pinia";
+import { ref, computed } from "vue";
 
-export const useCounterStore = defineStore({
-  id: "counter",
-  state: () => ({
-    count: 0,
-  }),
-  getters: {
-    getCount: (state) => state.count,
-    doubleCount: (state) => state.count * 2,
-    dividedBy: (state) => (divisor) => state.count / divisor,
-  },
-  actions: {
-    increment() {
-      this.count++;
-    },
-    decrement() {
-      this.count--;
-    },
-    incrementBy(number) {
-      this.count += number;
-    },
-  },
+export const useCounterStore = defineStore('counter', () => {
+  const count = ref(0);
+
+  const doubleCount = computed(() => count.value * 2);
+  const getCount = computed(() => count.value);
+  const dividedBy = computed(() => (divisor) => count.value / divisor);
+
+  function increment() {
+    count.value++;
+  }
+
+  function decrement() {
+    count.value--;
+  }
+
+  function incrementBy(number) {
+    count.value += number;
+  }
+
+  return {
+    count,
+    doubleCount,
+    increment,
+    decrement,
+    incrementBy,
+    getCount,
+    dividedBy
+  };
 });
--- src/stores/pokemon.js
+++ src/stores/pokemon.js
@@ -1,15 +1,21 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const usePokemonStore = defineStore({
-  id: "pokemon",
-  state: () => ({
-    pokemonDetails: {},
-  }),
-  actions: {
-    async getPokemonDetails(name) {
+export const usePokemonStore = defineStore('pokemon', () => {
+  const pokemonDetails = ref({});
+
+  async function getPokemonDetails(name) {
+    try {
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
       const result = await response.json();
-      this.pokemonDetails = result;
-    },
-  },
+      pokemonDetails.value = result;
+    } catch (error) {
+      console.error("Failed to fetch Pokemon details:", error);
+    }
+  }
+
+  return {
+    pokemonDetails,
+    getPokemonDetails,
+  };
 });
